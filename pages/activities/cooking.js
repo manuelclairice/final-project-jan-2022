8720de2cadf8dbe316392a190c8cd82790cee2fa
Manuel Clairice <manuel.clairice@gmail.com>
2022-04-01 11:44:12 +0200

    Modify picture extension to solve Heroku bug

--- pages/activities/cooking.js
+++ pages/activities/cooking.js
@@ -1,108 +1,168 @@
-import { css } from '@emotion/react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import Layout from '../../components/Layout';
+import { getActivityById, getClubById } from '../../util/database';
 
-const activitiesButtonsStyle = css`
-  display: flex;
-  margin-top: 20px;
-  justify-content: center;
-  text-align: center;
-
-  button {
-    border-radius: 100px;
-    padding: 30px;
-    border-width: 5px;
-    border-style: solid;
-    margin: 50px;
-    /* text-align: center; */
-    text-transform: uppercase;
-    display: inline-block;
-    font-size: 15px;
-    width: 120px;
-    height: 120px;
-
-    cursor: pointer;
-  }
-  a {
-    text-decoration: none;
-  }
-`;
-
-const artButtonStyle = css`
-  text-align: center;
-  font-weight: bold;
-  color: #fff;
-  background-color: #800080;
-  margin-right: 30px;
-`;
-const musicButtonStyle = css`
-  font-weight: bold;
-  color: #fff;
-  background-color: #ab0068;
-  margin-right: 30px;
-`;
-
-const danceButtonStyle = css`
-  font-weight: bold;
-  color: #fff;
-  background-color: #008080;
-  margin-right: 30px;
-`;
-const sportButtonStyle = css`
-  font-weight: bold;
-  color: #fff;
-  background-color: #088da5;
-`;
-
-export default function CookingPage(props) {
+export default function SingleActivity(props) {
   return (
     <Layout userObject={props.userObject}>
       <Head>
         <title>Vienna Kids Club</title>
         <meta
           name="description"
-          content="Find the best cooking club in Vienna for your children"
+          content="Find the best club in Vienna for your children"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image src="/cooking-hero.jpg" width="1600" height="500" />
       <div>
-        <h1>Cooking activities</h1>
+        <h1>{props.activity.name}</h1>
       </div>
-
-      <div css={activitiesButtonsStyle}>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
         <div>
-          <Link href="/activities/art">
-            <a>
-              <button css={artButtonStyle}>Art</button>
-            </a>
-          </Link>
+          <button>Get in touch</button>
         </div>
-        <div>
-          <Link href="/activities/music">
-            <a>
-              <button css={musicButtonStyle}>Music</button>
-            </a>
-          </Link>
-        </div>
-
-        <div>
-          <Link href="/activities/dance">
-            <a>
-              <button css={danceButtonStyle}>Dance</button>
-            </a>
-          </Link>
-        </div>
-        <div>
-          <Link href="/activities/sport">
-            <a>
-              <button css={sportButtonStyle}>Sport</button>
-            </a>
-          </Link>
-        </div>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
+      </div>
+      <div>
+        <Image
+          src={`/images/activities/${props.activity.name}.PNG`}
+          width="200"
+          height="200"
+        />
+        <div>{props.club.companyName}</div>
+        <div>{props.club.street}</div>
+        <div>{props.club.city}</div>
+        <div>{props.club.postCode}</div>
+        <div>{props.club.email}</div>
+        <div>{props.club.hourlyRate} euro/h</div>
+      </div>
+      <div>
+        <button>Get in touch</button>
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const activityId = context.query.activityId;
+
+  const activity = await getActivityById(activityId);
+  const club = await getClubById(activity.clubs_id);
+
+  console.log(activity);
+  console.log(activityId);
+
+  return {
+    props: {
+      activity,
+      activityId,
+      club,
+    },
+  };
 }
