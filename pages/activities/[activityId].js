6466cbe52cc6bc2f74397f554778fa2f7ec599f6
Manuel Clairice <manuel.clairice@gmail.com>
2022-04-02 19:12:45 +0200

    Remove react/bootstrap dependencie

--- pages/activities/[activityId].js
+++ pages/activities/[activityId].js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 import Layout from '../../components/Layout';
 import { getActivityById, getClubById } from '../../util/database';
 import { css } from '@emotion/react';
